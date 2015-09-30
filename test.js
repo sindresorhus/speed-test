60ab625a70b22faa764661637a50a2cb7328bac0
Sam Verschueren <sam.verschueren@gmail.com>
2015-09-30 13:29:05 +0200

    Added tests for --json and --verbose

--- test.js
+++ test.js
@@ -2,12 +2,60 @@
 var test = require('ava');
 var childProcess = require('child_process');
 
-test(function (t) {
+test('normal', function (t) {
 	t.plan(1);
 
 	var cp = childProcess.spawn('./cli.js', {
 		cwd: __dirname,
 		stdio: 'inherit'
+	});
+
+	cp.on('error', function (err) {
+		t.assert(!err, err);
+	});
+
+	cp.on('close', function (code) {
+		t.assert(code === 0);
+	});
+});
+
+test('--json', function (t) {
+	t.plan(1);
+
+	var cp = childProcess.spawn('./cli.js', ['--json'], {
+		cwd: __dirname
+	});
+
+	cp.stdout.on('data', function (data) {
+		data = JSON.parse(data.toString('utf8'));
+
+		if (data.ping === undefined || data.upload === undefined || data.download === undefined || data.upload !== undefined) {
+			cp.kill('SIGHUP');
+		}
+	});
+
+	cp.on('error', function (err) {
+		t.assert(!err, err);
+	});
+
+	cp.on('close', function (code) {
+		t.assert(code === 0);
+	});
+});
+
+test('--verbose --json', function (t) {
+	t.plan(1);
+
+	var cp = childProcess.spawn('./cli.js', ['--verbose', '--json'], {
+		cwd: __dirname
+	});
+
+	cp.on('data', function (data) {
+		data = JSON.parse(data.toString('utf8'));
+
+		if (data.ping === undefined || data.upload === undefined || data.download === undefined || data.upload === undefined) {
+			cp.kill('SIGHUP');
+		}
 	});
 
 	cp.on('error', function (err) {
