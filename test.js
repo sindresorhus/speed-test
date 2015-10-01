baa622473f0f2424bac3c7bb7f15334d73b0c45e
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-01 08:29:41 +0200

    Fixed test assertions

--- test.js
+++ test.js
@@ -20,49 +20,53 @@ test('main', function (t) {
 });
 
 test('--json', function (t) {
-	t.plan(1);
-
-	var cp = childProcess.spawn('./cli.js', ['--json'], {
+	var cp = childProcess.execFile('./cli.js', ['--json'], {
 		cwd: __dirname
 	});
 
+	cp.stdout.setEncoding('utf8');
 	cp.stdout.on('data', function (data) {
-		data = JSON.parse(data.toString('utf8'));
+		data = JSON.parse(data);
 
-		if (data.ping === undefined || data.upload === undefined || data.download === undefined || data.data !== undefined) {
-			cp.kill('SIGHUP');
-		}
+		t.false(data.ping === undefined);
+		t.false(data.upload === undefined);
+		t.false(data.download === undefined);
+		t.true(data.data === undefined);
 	});
 
 	cp.on('error', function (err) {
 		t.assert(!err, err);
+		t.end();
 	});
 
 	cp.on('close', function (code) {
 		t.assert(code === 0);
+		t.end();
 	});
 });
 
 test('--verbose --json', function (t) {
-	t.plan(1);
-
-	var cp = childProcess.spawn('./cli.js', ['--verbose', '--json'], {
+	var cp = childProcess.execFile('./cli.js', ['--verbose', '--json'], {
 		cwd: __dirname
 	});
 
+	cp.stdout.setEncoding('utf8');
 	cp.stdout.on('data', function (data) {
-		data = JSON.parse(data.toString('utf8'));
+		data = JSON.parse(data);
 
-		if (data.ping === undefined || data.upload === undefined || data.download === undefined || data.data === undefined) {
-			cp.kill('SIGHUP');
-		}
+		t.false(data.ping === undefined);
+		t.false(data.upload === undefined);
+		t.false(data.download === undefined);
+		t.false(data.data === undefined);
 	});
 
 	cp.on('error', function (err) {
 		t.assert(!err, err);
+		t.end();
 	});
 
 	cp.on('close', function (code) {
 		t.assert(code === 0);
+		t.end();
 	});
 });
