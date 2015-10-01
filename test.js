3be8c397df3a836d78d1747fa4e8d591780dde2f
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-01 20:45:49 +0700

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,53 +1,47 @@
-'use strict';
-var test = require('ava');
-var childProcess = require('child_process');
+import test from 'ava';
+import childProcess from 'child_process';
 
-test('main', function (t) {
+test('main', t => {
 	t.plan(1);
 
-	var cp = childProcess.spawn('./cli.js', {
+	const cp = childProcess.spawn('./cli.js', {
 		cwd: __dirname,
 		stdio: 'inherit'
 	});
 
-	cp.on('error', function (err) {
-		t.assert(!err, err);
-	});
-
-	cp.on('close', function (code) {
-		t.assert(code === 0);
-	});
+	cp.on('error', err => t.ifError(err));
+	cp.on('close', code => t.is(code, 0));
 });
 
-test('--json', function (t) {
-	childProcess.execFile('./cli.js', ['--json'], {cwd: __dirname, encoding: 'utf8'}, function (err, data) {
-		if (err) {
-			t.fail(err);
-		} else {
-			data = JSON.parse(data);
+test('--json', t => {
+	childProcess.execFile('./cli.js', ['--json'], {
+		cwd: __dirname
+	}, (err, data) => {
+		t.ifError(err);
 
-			t.false(data.ping === undefined);
-			t.false(data.upload === undefined);
-			t.false(data.download === undefined);
-			t.true(data.data === undefined);
-		}
+		data = JSON.parse(data);
+
+		t.not(data.ping, undefined);
+		t.not(data.upload, undefined);
+		t.not(data.download, undefined);
+		t.is(data.data, undefined);
 
 		t.end();
 	});
 });
 
-test('--verbose --json', function (t) {
-	childProcess.execFile('./cli.js', ['--verbose', '--json'], {cwd: __dirname, encoding: 'utf8'}, function (err, data) {
-		if (err) {
-			t.fail(err);
-		} else {
-			data = JSON.parse(data);
+test('--verbose --json', t => {
+	childProcess.execFile('./cli.js', ['--verbose', '--json'], {
+		cwd: __dirname
+	}, (err, data) => {
+		t.ifError(err);
 
-			t.false(data.ping === undefined);
-			t.false(data.upload === undefined);
-			t.false(data.download === undefined);
-			t.false(data.data === undefined);
-		}
+		data = JSON.parse(data);
+
+		t.not(data.ping, undefined);
+		t.not(data.upload, undefined);
+		t.not(data.download, undefined);
+		t.not(data.data, undefined);
 
 		t.end();
 	});
