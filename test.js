f6492183d172ffb5289e86c6016fab732b5c8669
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-01 08:45:42 +0200

    Fixed execFile in the tests

--- test.js
+++ test.js
@@ -20,53 +20,35 @@ test('main', function (t) {
 });
 
 test('--json', function (t) {
-	var cp = childProcess.execFile('./cli.js', ['--json'], {
-		cwd: __dirname
-	});
+	childProcess.execFile('./cli.js', ['--json'], {cwd: __dirname, encoding: 'utf8'}, function (err, data) {
+		if (err) {
+			t.fail(err);
+		} else {
+			data = JSON.parse(data);
 
-	cp.stdout.setEncoding('utf8');
-	cp.stdout.on('data', function (data) {
-		data = JSON.parse(data);
+			t.false(data.ping === undefined);
+			t.false(data.upload === undefined);
+			t.false(data.download === undefined);
+			t.true(data.data === undefined);
+		}
 
-		t.false(data.ping === undefined);
-		t.false(data.upload === undefined);
-		t.false(data.download === undefined);
-		t.true(data.data === undefined);
-	});
-
-	cp.on('error', function (err) {
-		t.assert(!err, err);
-		t.end();
-	});
-
-	cp.on('close', function (code) {
-		t.assert(code === 0);
 		t.end();
 	});
 });
 
 test('--verbose --json', function (t) {
-	var cp = childProcess.execFile('./cli.js', ['--verbose', '--json'], {
-		cwd: __dirname
-	});
+	childProcess.execFile('./cli.js', ['--verbose', '--json'], {cwd: __dirname, encoding: 'utf8'}, function (err, data) {
+		if (err) {
+			t.fail(err);
+		} else {
+			data = JSON.parse(data);
 
-	cp.stdout.setEncoding('utf8');
-	cp.stdout.on('data', function (data) {
-		data = JSON.parse(data);
+			t.false(data.ping === undefined);
+			t.false(data.upload === undefined);
+			t.false(data.download === undefined);
+			t.false(data.data === undefined);
+		}
 
-		t.false(data.ping === undefined);
-		t.false(data.upload === undefined);
-		t.false(data.download === undefined);
-		t.false(data.data === undefined);
-	});
-
-	cp.on('error', function (err) {
-		t.assert(!err, err);
-		t.end();
-	});
-
-	cp.on('close', function (code) {
-		t.assert(code === 0);
 		t.end();
 	});
 });
